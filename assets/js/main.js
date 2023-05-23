d8ee0ac4f9cf7b7d3fb7117ea783b6c7dd8584a9
Sam-Mucyo <90157786+Sam-Mucyo@users.noreply.github.com>
2023-05-23 14:51:11 -0700

    content update

--- assets/js/main.js
+++ assets/js/main.js
@@ -155,11 +155,18 @@ const selectedIcon = localStorage.getItem('selected-icon')
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
 
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+
+let date = new Date();
+let hour = date.getHours();
+
+if (hour < 6 || hour > 18) {
+    // If time is between 6PM (18:00) to 6AM (06:00), apply dark theme
+    document.body.classList.add('darkTheme');
+    themeButton.classList.add('iconTheme');
+} else {
+    // Otherwise (6AM to 6PM), remove the dark theme
+    document.body.classList.remove('darkTheme');
+    themeButton.classList.remove('iconTheme');
 }
 
 // Activate / deactivate the theme manually with the button
